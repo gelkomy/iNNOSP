dc08987a5d83f45f84c1eb759c7755f68aeb3259
yousefessam <yousef.essam.eldin@gmail.com>
2017-05-24 12:32:39 +0200

    modify test

--- test_kalman.py
+++ test_kalman.py
@@ -6,13 +6,11 @@ Created on Mon May 22 14:38:01 2017
 """
 
 from preprocessing import preprocessing
-from feature_extraction import feature_extraction
-from calculate_R import calculate_R
-from compute_j import compute_j
+
 from kalman_filter import kalman_filter
 import pandas as pd
-folder_dir=r'E:\Inno Tech\Patient 14 csv'
+folder_dir=r'E:\Inno Tech\Patient14_2'
 data=preprocessing(folder_dir)
-dataX = pd.DataFrame(data.ix[1:1000000,1]);
-res = kalman_filter(dataX);
+#dataX = pd.DataFrame(data.ix[1:100,1]);
+#res = kalman_filter(dataX);
 
