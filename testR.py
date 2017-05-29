bde5d4b23c57066526fef1a6f401e7811d1ca0b4
Emad24 <emadeldin_ahmed@hotmail.com>
2017-05-29 11:41:12 +0200

    Update files reading

--- testR.py
+++ testR.py
@@ -1,8 +1,14 @@
-import cPickle as pickle
+#import cPickle as pickle
 from calculate_R import calculate_R
+import pandas as pd
 
+#data = pickle.load( open( "../preprocessed.pkl", "rb" ) )
 
-data = pickle.load( open( "../preprocessed.pkl", "rb" ) )
+data={}
+d = pd.read_csv('C:\Users\Emad\Desktop\inF.csv')
+
+data['a'] = d
+
 R=calculate_R(data)
 
 print R
