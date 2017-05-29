39e8a45eaf7bee9c9b33163eb1bdf3b190fb5824
gelkomy <gamal.elkomy@innotech.com.eg>
2017-05-29 14:43:44 +0200

    test pycharm commit

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -4,7 +4,7 @@ from calculate_R import calculate_R
 
 
 import pickle
-x=pickle.load(open(r'E:\Faculty of Engineering\InnoTech\Epilipsy\Data\file1','rb'))
+x=pickle.load(open(r'E:\Faculty of Engineering\InnoTech\Epilipsy\Data\pkl patient 14\all_files.pkl','rb'))
 
 
 r = calculate_R(x)
