d64c149376a4d31b9136df2235142a5e5decad71
Emad24 <emadeldin_ahmed@hotmail.com>
2017-05-30 16:09:34 +0200

    some updates

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -4,21 +4,22 @@ from calculate_R import calculate_R
 
 
 import pickle
-x=pickle.load(open(r'E:\Faculty of Engineering\InnoTech\Epilipsy\Data\file1','rb'))
+x=pickle.load(open(r'D:\College\Inno\Python code\all_files_hp.pkl','rb'))
 
 
 r = calculate_R(x)
 from PrepareYVector import getY
 from PrepareWindowsVector import getWindows
 from feature_basis_selection import feature_basis_selection
+from electrode_selection import electrode_selction
 
-text_file=r'E:\Faculty of Engineering\InnoTech\Epilipsy\Data\Patient14\chb14-summary.txt'
-yTimeLine = getY(text_file,1 ,1 )
+text_file=r'D:\College\Inno\InnoBCI\data\Patient14\chb14-summary.txt'
+yTimeLine = getY(text_file,1 ,100 )
 #
 y = getWindows(yTimeLine)
 #
 #
 result=feature_basis_selection(x,y,r)
-
+sel_elec = electrode_selction(result, y)
 
 print r
